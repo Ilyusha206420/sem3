c0f9c07745f45480f7131beb0fc5234c67a974d0
Shefer Ilya <shefer-2006@mail.ru>
2025-12-16 19:56:00 +0300

    Error handling

--- OP/Cpreprocessor/src/main.c
+++ OP/Cpreprocessor/src/main.c
@@ -10,7 +10,29 @@ int main(int argc, char **argv)
     in = argv[1];
   if (argc >= 3) 
     out = argv[2];
-  printf("%d\n", processFile((char*)in, (char*)out));
+  int error = processFile(in, out);
+  switch (error) {
+    case 0:
+    break;
+    case FILE_OPEN_ERROR:
+      printf("Error! Can't open file: %s\n", in);
+    break;
+    case FILE_CREATE_ERROR:
+      printf("Error! Can't create file: %s\n", out);
+    break;
+    case MEMORY_ALLOC_ERROR:
+      printf("Error! Too little RAM\n");
+    break;
+    case HEADER_FILE_OPENINNG_ERROR:
+      printf("Error! Can't open included file!\n");
+    break;
+    case STACK_ERROR:
+      printf("Error! Can't allocate memory for file stack!\n");
+    break;
+    default:
+      printf("Error! Unexpected error\n");
+    break;
+  }
 
-  return 0;
+  return error;
 }
